29a8dc043135f7395decf17d77cc1966d359b1f3
nOOb3167 <github@desu.no-ip.info>
2011-11-29 23:56:34 +0100

    Add stage.h

--- src/stage.c
+++ src/stage.c
@@ -1,19 +1,4 @@
-#include <glib.h>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_opengl.h>
-
-struct DepthData
-{
-  GLuint fbo;
-  GLuint ab;
-  GLuint cat0_loc;
-
-  gint _dtest;
-  gint _dfunc;
-  gint _dclear;
-  gint _dmask;
-  gint _cmask[4];
-};
+#include <src/stage.h>
 
 /**
  * Garbles:
---
+++ src/stage.h
@@ -0,0 +1,19 @@
+#include <glib.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_opengl.h>
+
+struct DepthData
+{
+  GLuint fbo;
+  GLuint ab;
+  GLuint cat0_loc;
+
+  gint _dtest;
+  gint _dfunc;
+  gint _dclear;
+  gint _dmask;
+  gint _cmask[4];
+};
+
+void depth_bind (struct DepthData ds);
+void depth_unbind (struct DepthData ds);
