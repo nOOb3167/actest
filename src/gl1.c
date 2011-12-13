6657a6a8180013d536e19af6eb936c381fd8effb
nOOb3167 <github@desu.no-ip.info>
2011-12-13 05:51:37 +0100

    gl1 remove code

--- src/gl1.c
+++ src/gl1.c
@@ -54,191 +54,6 @@ debug_draw_tex_quad (GLuint tex, float x, float y, float w, float h)
   check_gl_error ();
 }
 
-int
-make_fbo (void)
-{
-  check_gl_error ();
-
-  GLenum err;
-  
-  GLuint tex;
-  GLuint tex2;
-
-  glGenTextures (1, &tex);
-  glActiveTexture (GL_TEXTURE0);
-  glBindTexture (GL_TEXTURE_2D, tex);
-  glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexImage2D (GL_TEXTURE_2D, 0, GL_RGBA,
-		640, 480, 0,
-		GL_RGBA, GL_UNSIGNED_BYTE,
-		NULL);
-
-  glGenTextures (1, &tex2);
-  glActiveTexture (GL_TEXTURE0);
-  glBindTexture (GL_TEXTURE_2D, tex2);
-  glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexImage2D (GL_TEXTURE_2D, 0, GL_RGBA,
-		640, 480, 0,
-		GL_RGBA, GL_UNSIGNED_BYTE,
-		NULL);
-
-  GLuint fbo;
-
-  glGenFramebuffersEXT (1, &fbo);
-  glBindFramebufferEXT (GL_FRAMEBUFFER, fbo);
-  glFramebufferTexture2DEXT (GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
-			     GL_TEXTURE_2D, tex, 0);
-
-  glFramebufferTexture2DEXT (GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1,
-			     GL_TEXTURE_2D, tex2, 0);
-
-  xassert (GL_FRAMEBUFFER_COMPLETE ==
-	   glCheckFramebufferStatusEXT (GL_FRAMEBUFFER));
-
-  check_gl_error ();
-
-  GLint status;
-
-  GLuint vshd;
-  GLuint fshd;
-
-  vshd = glCreateShader (GL_VERTEX_SHADER);
-  glShaderSource (vshd, 1, vshd_src, NULL);
-  glCompileShader (vshd);
-  
-  glGetShaderiv (vshd, GL_COMPILE_STATUS, &status);
-  xassert (GL_TRUE == status);
-
-  fshd = glCreateShader (GL_FRAGMENT_SHADER);
-  glShaderSource (fshd, 1, fshd_src, NULL);
-  glCompileShader (fshd);
-  
-  glGetShaderiv (fshd, GL_COMPILE_STATUS, &status);
-  xassert (GL_TRUE == status);
-
-  GLuint prog;
-
-  check_gl_error ();
-
-  prog = glCreateProgram ();
-  glAttachShader (prog, vshd);
-  glAttachShader (prog, fshd);  
-  glLinkProgram (prog);
-  
-  glGetProgramiv (prog, GL_LINK_STATUS, &status);
-  xassert (GL_TRUE == status);
-
-  check_gl_error ();
-
-  GLint cat0_loc;
-
-  cat0_loc = glGetAttribLocation (prog, "cat0");
-  xassert (-1 != cat0_loc);
-
-  check_gl_error ();
-
-  GLuint ab;
-
-  GLubyte *abbuf;
-  GLfloat *abbuf_f;
-  abbuf = malloc (4*4 * 3);
-  abbuf_f = (GLfloat *)abbuf;
-  xassert (abbuf);
-
-  abbuf_f[0] = 0.0f;
-  abbuf_f[1] = 0.0f;
-  abbuf_f[2] = 0.0f;
-  abbuf_f[3] = 1.0f;
-  abbuf_f[4] = 100.0f;
-  abbuf_f[5] = 0.0f;
-  abbuf_f[6] = 0.0f;
-  abbuf_f[7] = 1.0f;
-  abbuf_f[8] = 50.0f;
-  abbuf_f[9] = 100.0f;
-  abbuf_f[10] = 0.0f;
-  abbuf_f[11] = 1.0f;
-
-  glGenBuffers (1, &ab);
-  glBindBuffer (GL_ARRAY_BUFFER, ab);
-  glBufferData (GL_ARRAY_BUFFER,
-		4*4 * 3, abbuf,
-		GL_STREAM_DRAW);
-
-  /* glVertexAttribPointer acts on the buffer object currently
-     bound to ARRAY_BUFFER_BINDING, the buffer object
-     becomes 'baked' into VERTEX_ATTRIB_ARRAY_BUFFER_BINDING
-     for the specified index (cat0_loc in this case) */
-  /* For indices, also use ELEMENT_ARRAY_BUFFER, with DrawElements (2.9.7) */
-  glVertexAttribPointer (cat0_loc, 4, GL_FLOAT, GL_FALSE, 0, 0);
-
-  /* Had a bug: Calling with 0 instead of cat0_loc */
-  glEnableVertexAttribArray (cat0_loc);
-
-  check_gl_error ();
-
-  glMatrixMode (GL_MODELVIEW);
-  glLoadIdentity ();
-
-  glBindFramebufferEXT (GL_FRAMEBUFFER_EXT, fbo);
-
-  {
-    glPushAttrib (GL_VIEWPORT_BIT | GL_TRANSFORM_BIT);
-    glViewport (0, 0, 640, 480);
-    glClearColor (0, 1, 0, 1);
-    glClear (GL_COLOR_BUFFER_BIT);
-
-    {
-      glMatrixMode (GL_PROJECTION);
-      glPushMatrix ();
-      glLoadIdentity ();
-      glOrtho (0, 256, 256, 0, -1, 1);
-  
-      GLenum draw_buffers[] = {GL_COLOR_ATTACHMENT0,
-			       GL_COLOR_ATTACHMENT1};
-      glDrawBuffers (2, draw_buffers);
-      glUseProgram (prog);
-
-      glDrawArrays (GL_TRIANGLES, 0, 3);
-
-      glMatrixMode (GL_PROJECTION);
-      glPopMatrix ();
-    }
-
-    glPopAttrib ();
-  }
-
-  glBindFramebufferEXT (GL_FRAMEBUFFER_EXT, 0);
-
-  check_gl_error ();
-
-  glClearColor (0, 0, 1, 1);
-  glClear (GL_COLOR_BUFFER_BIT);
-
-  glMatrixMode (GL_MODELVIEW);
-  glLoadIdentity ();
-
-  glEnable (GL_TEXTURE_2D);
-  glUseProgram (0);
-
-  glBindTexture(GL_TEXTURE_2D, tex);
-  glBegin(GL_QUADS);
-  glTexCoord2f(0, 0); glVertex2f(0, 0);
-  glTexCoord2f(1, 0); glVertex2f(100, 0);
-  glTexCoord2f(1, 1); glVertex2f(100, 100);
-  glTexCoord2f(0, 1); glVertex2f(0, 100);
-  glEnd();
-
-  glBindTexture(GL_TEXTURE_2D, tex2);
-  glBegin(GL_QUADS);
-  glTexCoord2f(0, 0); glVertex2f(0, 150);
-  glTexCoord2f(1, 0); glVertex2f(100, 150);
-  glTexCoord2f(1, 1); glVertex2f(100, 250);
-  glTexCoord2f(0, 1); glVertex2f(0, 250);
-  glEnd();
-}
-
 void
 mesh_arrays_extract (struct aiMesh *me,
                      GLfloat **v,
@@ -378,6 +193,12 @@ derp (void)
   g_xassert (aiPrimitiveType_TRIANGLE == me->mPrimitiveTypes);
 
   mesh_buffers_extract (me, &vb, &nb, &tb);
+
+  /* Clear the default framebuffer (Optional) */
+
+  glBindFramebufferEXT (GL_FRAMEBUFFER_EXT, 0);
+  glClearColor (0, 0, 1, 1);
+  glClear (GL_COLOR_BUFFER_BIT);
 
   /* Create two textures, a color and a depth. */
 
@@ -586,10 +407,6 @@ main (int argc, char **argv)
   xassert (4 <= glvar);
   glGetIntegerv(GL_MAX_COLOR_ATTACHMENTS, &glvar);
   xassert (4 <= glvar);
-
-  /* mmstuff (); */
-
-  make_fbo ();
 
   derp ();
 
